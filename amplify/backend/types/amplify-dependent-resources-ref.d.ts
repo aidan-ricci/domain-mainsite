7515a1811125260a6da0fa3417a8585ba8af6cd8
Aidan Ricci <aiqr25@gmail.com>
2025-06-23 22:10:10 -0400

    added analytics

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "domainmainsite": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "domainmainsite": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -17,7 +17,7 @@ const itemVariants = {
     opacity: 1,
     y: 0,
     transition: {
-      type: "spring",
+      type: "spring" as const,
       stiffness: 50,
     },
   },
